4e6b452b988fe6599ac5fef191b620396de05075
Ivan Cheklin <ivan.cheklin.1985@gmail.com>
2022-07-02 22:06:06 +0300

    Seminar1_task0 added

---
+++ Seminar1/task_0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number * number;
+Console.Write($"Квадрат числа {number} равен {result}");
